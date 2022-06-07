d1171742cefef0b01eca33932d2cac41e8f1db0f
Flay000 <flayrys100@gmail.com>
2022-06-07 16:56:11 -0300

    3 quase feita

---
+++ script.js
@@ -0,0 +1,19 @@
+const formBtn = document.getElementById("loginBtn");
+const email = document.getElementById("email");
+const password = document.getElementById("senha");
+
+function checkLogin(){
+
+  const emailValue = 'tryber@teste.com';
+  const passwordValue = "123456";
+
+  if(email.value === emailValue && password.value === passwordValue){
+ alert("Olá, Tryber!");
+  }else{
+    alert ("Email ou senha inválidos");
+  }
+};
+
+formBtn.addEventListener("click", checkLogin);
+
+
